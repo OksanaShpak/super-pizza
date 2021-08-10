bc2487a3cf8c2efb754dda49f58d25f88f8fdb16
Oksana <oksana.shpack@gmail.com>
2021-08-10 18:15:39 +1000

    add routing to home and cart pages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/app.scss";
 import App from "./App";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route exact path="/" component={App} />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
