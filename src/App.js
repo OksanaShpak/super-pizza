11abb5853901b04e2def8fa1185dd2083adaa9cf
Oksana <oksana.shpack@gmail.com>
2021-08-10 22:08:44 +1000

    add fake server comunication, name, image, price and type for pizza block

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { Route } from "react-router-dom";
 import { Header } from "./components";
 import { Cart, Home } from "./pages";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/fake-server.json")
+      .then((response) => response.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
+  console.log(pizzas);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content"></div>
-      <Route path="/" component={Home} exact/>
-      <Route path="/cart" component={Cart} exact/>
+      <Route path="/" render={() => <Home items={pizzas}/>} exact />
+      <Route path="/cart" component={Cart} exact />
     </div>
   );
 }
